739f2a196f1158d59f1d0e50c5f938e86453e42e
ash-shaik <fastmlai777@gmail.com>
2021-12-01 12:29:00 -0800

    Sketch Algorithms

---
+++ prelim/lossy-summarization.py
@@ -0,0 +1,24 @@
+"""
+ Sketch algorithms were developed to approximate answers to questions such as:
+ 1. Set membership
+ 2. Cardinality estimation
+ 3. Frequency.
+
+ 1. Update in the order of O(1)
+ 2. Storage space is independent of the size of the stream
+ 3. Read in linear time.
+
+ Downside:
+ 1. Tolerable Errors in results.
+
+ Algorithms:
+ 1. Bloom Filters.
+ 2. Min Count
+ 3. HyperLogLog
+ 4. Count-Min Sketch
+
+
+
+
+
+"""
