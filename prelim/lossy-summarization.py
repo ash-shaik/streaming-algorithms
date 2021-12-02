cf128b2e9ec75eb1f87f68bae6fda6c463dd73a7
ash-shaik <fastmlai777@gmail.com>
2021-12-01 16:06:53 -0800

    Sketch Algorithms: Bloom filter skeleton

--- prelim/lossy-summarization.py
+++ prelim/lossy-summarization.py
@@ -17,8 +17,17 @@
  3. HyperLogLog
  4. Count-Min Sketch
 
+Building Blocks:
+1. Registers - counters that store the data for the sketch.
+   Does not depend on the number of input elements.
+2. Hash Functions - Deterministic functions that take an input and return a value in some finite
+   space of m output values. Speed is the primary requirement of these functions in application
+   of sketches.
+   Murmur hash is a popular variant used.
+"""
 
-
-
-
+"""
+Bloom Filter - Data Structure used by a applications to store set membership information.
+Trade Off - Possible False Positives
+The size of the data strucure allows us to control the false positive rate.
 """
