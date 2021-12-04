88f71617a93d3e4781e1e6a13b2ed9f44d894a82
ash-shaik <fastmlai777@gmail.com>
2021-12-03 19:04:04 -0800

    Aggregations usecase : flow metrics

--- sparkstreaming-kafka/src/main/scala/use-cases/FlowMetrics.scala
+++ sparkstreaming-kafka/src/main/scala/use-cases/FlowMetrics.scala
@@ -1,59 +1,75 @@
 
+import org.slf4j.LoggerFactory
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{avg, datediff, col}
+import org.apache.spark.sql.functions.{avg, col, datediff}
 
-val PROJECT_NAME = "yourprojectname"
+val PROJECT_NAME: String = "your_project_name"
 
-object FlowMetrics{
+class FlowMetricsApp {
 
-  /**
-    * Application entry point
-    * @param args
-    */
-   def main(args: Array[String]): Unit = {
+  private val log: org.slf4j.Logger = LoggerFactory.getLogger(Predef.classOf[FlowMetricsApp])
 
+  def start(): Unit = {
     //create a session
-    val spark = SparkSession.builder()
+    val spark: SparkSession = SparkSession.builder()
       .appName("Flow Metrics")
       .master("local[*]")
       .getOrCreate()
 
-    val valueStreamDF = spark.read.format("bigquery")
+    val valueStreamDF: _root_.org.apache.spark.sql.DataFrame = spark.read.format("bigquery")
       .option("credentialsFile", "src/main/resources/key/{PROJECT_NAME}.json")
       .option("table", "flow_models:items")
       .load()
       .cache()
 
-     valueStreamDF.createOrReplaceTempView("flow_item_summary")
+    valueStreamDF.createOrReplaceTempView("flow_item_summary")
 
-     // using spark sql.
-     val flowTimeSQL = "SELECT project, flow_item, AVG(DATE_DIFF(closed_at, started_at)) as avg_flow_time_days " +
-       "FROM flow_item_summary GROUP BY project, flow_item"
+    log.debug("Num records in valueStream : {}", valueStreamDF.count())
 
-     val flowTimeDF = spark.sql(flowTimeSQL)
+    // using spark sql.
+    val flowTimeSQL: String = "SELECT project, flow_item, AVG(DATE_DIFF(closed_at, started_at)) as avg_flow_time_days ".+("FROM flow_item_summary GROUP BY project, flow_item")
 
-     //Using dataframe API
-     val avgFlowTimeDF = valueStreamDF.withColumn("flow_time_days"
-       , datediff(col("closed_at"), col("started_at")))
-       .groupBy(col("project"), col("flow_item")).agg(
-       avg("flow_time_days")
-     )
+    val flowTimeDF: _root_.org.apache.spark.sql.DataFrame = spark.sql(flowTimeSQL)
+    flowTimeDF.show(20)
 
-     avgFlowTimeDF.show(20)
-     
+    //Using dataframe API
+    val avgFlowTimeDF: _root_.org.apache.spark.sql.DataFrame = valueStreamDF.withColumn("flow_time_days"
+      , datediff(col("closed_at"), col("started_at")))
+      .groupBy(col("project"), col("flow_item")).agg(
+      avg("flow_time_days")
+    )
+
+    avgFlowTimeDF.show(20)
 
   }
 
+
   /*
 
-    Schema -
-    flow_item, flow_item_type, created_at, updated_at, status, assignee, project
+      Schema -
+      flow_item, flow_item_type, created_at, updated_at, status, assignee, project
 
-    Table : - flow_item_summary
-    flow_item, flow_item_type, started_at, closed_at, assignee, project
+      Table : - flow_item_summary
+      flow_item, flow_item_type, started_at, closed_at, assignee, project
 
-   */
+     */
 
+}
+
+
+object FlowMetrics {
+
+
+  /**
+    * Application entry point
+    *
+    * @param args command line arguments
+    */
+  def main(args: Array[String]): Unit = {
+    val app: FlowMetricsApp = new FlowMetricsApp
+    app.start()
+
+  }
 
 
 }
