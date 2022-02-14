87be4c98625c443f9b2bcf4729d2f71d85a0ff18
ash-shaik <fastml777@gmail.com>
2022-02-14 14:11:33 -0800

    generators

--- misc/lazy_iterator.py
+++ misc/lazy_iterator.py
@@ -6,6 +6,16 @@
 
 
 def sequence_enumerator(my_data):
+    """
+    The yield statement turns a functions into a generator.
+    A generator is a function which returns a generator object.
+    As soon as "next" is called again on the generator object,
+    the generator function will resume execution right after
+    the yield statement in the code, where the last call is made.
+    The execution will continue in the state in which the generator was left after the last yield
+    :param my_data:
+    :return:
+    """
     index = 0
     while True:
         yield index, my_data[index]
